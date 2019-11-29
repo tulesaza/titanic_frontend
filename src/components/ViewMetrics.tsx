fc2947eddb22e9ae3c1b4c561aaf1d3536756ee5
azamat <tuleshov.azamat13@gmail.com>
2019-11-29 16:27:40 +0100

    chart.js test

--- src/components/ViewMetrics.tsx
+++ src/components/ViewMetrics.tsx
@@ -3,6 +3,8 @@ import {Button} from "@material-ui/core";
 import {ChildrensProps} from "./UploadDataset";
 import {restClient} from "../index";
 import {States} from "./MainPage";
+import {ChartOptions} from "chart.js";
+import {Pie} from 'react-chartjs-2'
 
 interface ViewMetricsState {
   loading: boolean;
@@ -40,12 +42,39 @@ export class ViewMetrics extends React.Component<
   }
 
   render() {
+      const data = {
+      labels: [
+        'Red',
+        'Green',
+        'Yellow'
+      ],
+      datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ]
+      }]
+    };
+
+    const options: ChartOptions = {
+      legend: {
+        position: 'bottom',
+      }
+    };
     return this.state.loading ? (
       <div>
         <label>Loading...</label>
       </div>
     ) : (
       <div>
+          <Pie data={data} options={options} />
         <Button onClick={this.handleReset}>Reset</Button>
       </div>
     );
